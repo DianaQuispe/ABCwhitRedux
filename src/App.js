5529b4205a20a7e28223d20d3d5559285539db62
DianaQuispe <danaalienmas@gmail.com>
2017-10-24 14:40:14 -0500

    arreglar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React, { Component } from "react";
 import { connect } from "redux-zero/react";
-import { selectOption } from "./actions";
+import { selectOption, submit } from "./actions";
 import "./App.css";
 
 const Modal = ({}) => {
-  return <div className="modal fade" id="exampleModal" tabIndex={-1} role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  return (
+    <div
+      className="modal fade"
+      id="exampleModal"
+      tabIndex={-1}
+      role="dialog"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
       <div className="modal-dialog" role="document">
         <div className="modal-content">
           <div className="modal-header">
             <h5 className="modal-title" id="exampleModalLabel">
               Do you want to learn how to build React apps like this?
             </h5>
-            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+            <button
+              type="button"
+              className="close"
+              data-dismiss="modal"
+              aria-label="Close"
+            >
               <span aria-hidden="true">×</span>
             </button>
           </div>
@@ -25,8 +38,9 @@ const Modal = ({}) => {
           </div>
         </div>
       </div>
-    </div>;
-}
+    </div>
+  );
+};
 const Header = ({ game, currentQuestion }) => {
   return (
     <div>
@@ -40,84 +54,112 @@ const Header = ({ game, currentQuestion }) => {
           <i className="fa fa-info-circle fa-3x" aria-hidden="true" />
         </a>
       </div>
-
     </div>
   );
 };
-  const onSubmit = e => {
-    e.preventDefault();
-    selectOption(e);
-  }    
-const Questions = ({ game, currentQuestion, complet }) => {
-    const answers = game[currentQuestion].answers.map(thisAn => 
-      <form onSubmit={onSubmit}>
-          <div className="col-md-4">
-            <button 
-             ref={e => (this.refInput = e)}
-              className="btn btnQuiz btn-default btn-block">
-              <i className="fa fa-user" />
-              {thisAn}
-            </button>
-          </div>
-      </form>);
-
-  return <section className="contenedor container-fluid text-center">
-      <div className="row">
-        {complet && <YourAnswers/>}
-        <img id="imagenes" height={250} width={250} src={game[currentQuestion].image} className="img-responsive center-block" />
-        <div id="questions" />
-        {game[currentQuestion].question}
-        <div className="col-md-12 container-fluid" />
-        <div className="answers row">
-          {answers}
-        </div>
+const YourAnswers = ({ game, currentQuestion, complet, answers }) => {
+  const myQuestions = game.map((question,i) => {
+    return (
+      <div>
+        <ol>
+          <li>{question.question} 
+          <b> {answers[i]}</b>
+          </li>
+        </ol>
       </div>
-      <div className="row">
-        <div className="col-md-12">
-          <div id="divInterno">
-            <div className="social text-center">
-              <a href="#" className="btn btn-default btn-circle">
-                <i className="fa fa-twitter" />
-              </a>
-              <a href="#" className="btn btn-default btn-circle">
-                <i className="fa fa-facebook" />
-              </a>
-              <a href="#" className="btn btn-default btn-circle">
-                <i className="fa fa-google-plus" />
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>;
-};
-const YourAnswers= ({game,answers, currentQuestion}) => {
-  const myQuestions = game.map((question) => {
-  return(
-  <div>
-    <ol>
-      <li>
-        {question.question}
-      </li>
-    </ol>
-  </div>
-  )
-  })
-  return(
+    );
+  });
+  return (
     <div>
       <h1>Here are your answers:</h1>
-      {myQuestions}
-      {answers[currentQuestion]}
+      {myQuestions} 
+      <button className="btn btn-block" onClick={submit}>Submit</button>       
     </div>
-  )
-}
-const App = ({ game, currentQuestion,answers, complet }) => {
-  return <div>
-      <Header game={game} currentQuestion={currentQuestion} />
-      <Modal />
-      <Questions game={game} currentQuestion={currentQuestion} complet={complet} />
-    </div>;
+  );
+};
+const onSubmit = e => {
+  e.preventDefault();
+  selectOption(e);
 };
 
-const mapToProps = ({ game, currentQuestion, answers, complet }) => ({ game, currentQuestion, answers, complet });
+const Questions = ({ game, currentQuestion, complet, answers }) => {
+
+  
+    if(complet == true) {
+     return  <YourAnswers game={game} currentQuestion={currentQuestion} complet={complet} answers={answers} />
+    }else {
+      let answers;
+           answers = game[currentQuestion].answers.map(thisAn => (
+            <form onSubmit={onSubmit}>
+              <div className="col-md-4">
+                <button
+                  ref={e => (this.refInput = e)}
+                  className="btn btnQuiz btn-default btn-block"
+                >
+                  <i className="fa fa-user" />
+                  {thisAn}
+                </button>
+              </div>
+            </form>
+          ));
+        
+      return (
+        <section className="contenedor container-fluid text-center">
+          <div className="row">
+            <img
+              id="imagenes"
+              height={250}
+              width={250}
+              src={game[currentQuestion].image}
+              className="img-responsive center-block"
+            />
+            <div id="questions" />
+            {game[currentQuestion].question}
+            <div className="col-md-12 container-fluid" />
+            <div className="answers row">{answers}</div>
+          </div>
+          <div className="row">
+            <div className="col-md-12">
+              <div id="divInterno">
+                <div className="social text-center">
+                  <a href="#" className="btn btn-default btn-circle">
+                    <i className="fa fa-twitter" />
+                  </a>
+                  <a href="#" className="btn btn-default btn-circle">
+                    <i className="fa fa-facebook" />
+                  </a>
+                  <a href="#" className="btn btn-default btn-circle">
+                    <i className="fa fa-google-plus" />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      );
+    }
+ 
+};
+
+const App = ({ game, currentQuestion, answers, complet }) => {
+  return (
+    <div>
+      <Header game={game} currentQuestion={currentQuestion} />
+      <Modal />
+      <Questions
+        game={game}
+        currentQuestion={currentQuestion}
+        complet={complet}
+        answers={answers}
+      />
+    </div>
+  );
+};
+
+const mapToProps = ({ game, currentQuestion, answers, complet }) => ({
+  game,
+  currentQuestion,
+  answers,
+  complet
+});
 export default connect(mapToProps)(App);
--- src/actions.js
+++ src/actions.js
@@ -1,32 +1,36 @@
 import store from "./store";
-export const selectOption = (option) =>{
-    // console.log(option.target.textContent)
-    let thisAnswer = option.target.textContent;
-    let answers = store.getState().answers;
-    let push =  answers.push(thisAnswer)
-    let currentQuestion = store.getState().currentQuestion
-    let answerGood = [];
-    let thatAnswer = store.getState().game[store.getState().currentQuestion].answerGood;
-    answerGood.push(thatAnswer);
-    console.log(answers, answerGood);
-
-    currentQuestion++;
-    if(push == store.getState().game.length) {
-        store.setState({
-            complet : true,
-        })
-        console.log(store.getState().complet);
-    }
+export const selectOption = option => {
+  let answers = store.getState().answers;
+  let push = answers.push(option.target.textContent);
+  let currentQuestion = store.getState().currentQuestion;
+  let answerGood = [];
+  let thatAnswer = store.getState().game[store.getState().currentQuestion].answerGood;
+  answerGood.push(thatAnswer);
+  console.log(answers, answerGood);
+  let cont = 0;
+  
+  if (answers[currentQuestion] == answerGood) {
+      console.log('asd')
+      cont++;
+  }else {
+      console.log('s')
+  }
+  console.log(cont)
+  
+  currentQuestion++;
+  if (push == store.getState().game.length) {
     store.setState({
-      currentQuestion:  currentQuestion,
-      answers: answers
+      complet: true
     });
+    console.log(store.getState().complet);
+  }
+  store.setState({
+    currentQuestion: currentQuestion,
+    answers: answers
+  });
 
 
-//     if (answers[currentQuestion] == store.getState().answerGood[currentQuestion]) {
-//       console.log("equals:", answers[store.getState().currentQuestion]);
-//     } else {
-//       console.log(answers);
-//     }
+};
+export const submit = () => {
 
 };
